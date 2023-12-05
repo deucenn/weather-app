e82b439a2b4dad92be64a6305e67f8d035cb344f
deucenn <panix.black@web.de>
2023-12-05 18:35:52 +0100

    version 1.0

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const config = {
+    apiKey: 'eb723a7c39b8022902ad77878321056b'
+  };
+  
---
+++ main.js
@@ -0,0 +1,40 @@
+const locationIn = document.getElementById("locationName");
+
+
+apik = "eb723a7c39b8022902ad77878321056b";
+
+function conversion(val) {
+  return Math.floor(val - 273);
+}
+
+// https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api}
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        locationIn.value +
+        "&appid=" +
+        config.apiKey
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const tempMarkup = `<h2>${conversion(data.main.temp)}°C</h2>`;
+        const skyMarkup = `<h3>${data.weather[0].main}</h3>`;
+        const locationMarkup = `<h4>${data.name}</h4>`;
+
+        document
+          .getElementById("temp")
+          .insertAdjacentHTML("beforeend", tempMarkup);
+        document
+          .getElementById("sky")
+          .insertAdjacentHTML("beforeend", skyMarkup);
+        document
+          .getElementById("location")
+          .insertAdjacentHTML("beforeend", locationMarkup);
+      })
+      .catch((error) => alert("Wrong city name"));
+
+    locationName.value = "";
+  }
+});
